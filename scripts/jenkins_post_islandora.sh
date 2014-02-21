ffd4fea17b97aab305ca36621f1e43b7b42adfce
Kevin S. Clarke <ksclarke@gmail.com>
2014-02-21 12:04:55 -0500

    added our jenkins pre- and post-build scripts

---
+++ scripts/jenkins_post_islandora.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+###########################################################
+# Script Jenkins can run to finalize a new code update    #
+#                                                         #
+# Written by: Kevin S. Clarke <ksclarke@library.ucla.edu> #
+###########################################################
+
+DRUPAL_SITES="default drupal_site_1 drupal_site_2 drupal_site_3 drupal_site_4 drupal_site_5 drupal_site_7"
+WEB_USER=apache
+DRUPAL_HOME=/var/www/drupal
+
+if [ $# -eq 1 ] ; then
+  DRUPAL_SITES="$1"
+fi
+
+# Still need to do this though...
+find /var/www/drupal -name files | xargs sudo chown -R apache:apache
+
+# Reinstate original state of Drupal modules
+for SITE in ${DRUPAL_SITES}
+do
+  drush -y --root=${DRUPAL_HOME} -l ${SITE} fr-all --force
+  if [ $? -ne 0 ] ; then
+    exit $?
+  fi
+  drush -y --root=${DRUPAL_HOME} -l ${SITE} en `cat ${DRUPAL_HOME}/${SITE}-modules.txt`
+  if [ $? -ne 0 ] ; then
+    exit $?
+  fi
+  drush -y --root=${DRUPAL_HOME} -l ${SITE} updatedb
+  if [ $? -ne 0 ] ; then
+    exit $?
+  fi
+  drush -y --root=${DRUPAL_HOME} -l ${SITE} cc all
+  if [ $? -ne 0 ] ; then
+    exit $?
+  fi
+done
---
+++ scripts/jenkins_pre_islandora.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+###########################################################
+# Script Jenkins can run to prep for a new code update    #
+#                                                         #
+# Written by: Kevin S. Clarke <ksclarke@library.ucla.edu> #
+###########################################################
+
+DRUPAL_SITES="default drupal_site_1 drupal_site_2 drupal_site_3 drupal_site_4 drupal_site_5 drupal_site_7"
+JENKINS_USER=devUser1
+DRUPAL_HOME=/var/www/drupal
+
+# Create the module directory if it doesn't already exist
+if [ -n "$1" ] ; then
+  if [ ! -e "$1" ] ; then
+    FILE_PATH=`echo $1 | sed -e 's/[^a-zA-Z0-9\-\/]/_/g'`
+    sudo mkdir -p "$DRUPAL_HOME/$FILE_PATH"
+    if [ -e "$FILE_PATH" ] ; then
+      echo "New module directory created: $FILE_PATH"
+    fi
+  fi
+fi
+
+if [ $# -eq 2 ] ; then
+  DRUPAL_SITES="$2"
+fi
+
+# Change ownership of specified files and dirs to our user
+echo "Changing ownership of ${DRUPAL_HOME}/${1} to ${JENKINS_USER}"
+sudo chown -R ${JENKINS_USER} ${DRUPAL_HOME}/${1}
+if [ $? -ne 0 ] ; then
+  exit $?
+fi
+
+# Record the state of Drupal modules prior to new code update
+# Note: the db for the new site should have already been set up
+for SITE in ${DRUPAL_SITES}
+do
+  drush -y -r ${DRUPAL_HOME} -l ${SITE} pml --status=enabled --type=module --no-core --pipe > ${DRUPAL_HOME}/${SITE}-modules.txt
+  if [ $? -ne 0 ] ; then
+    exit $?
+  fi
+  drush -y -r ${DRUPAL_HOME} -l ${SITE} dis `cat ${DRUPAL_HOME}/${SITE}-modules.txt`
+  if [ $? -ne 0 ] ; then
+    exit $?
+  fi
+done
