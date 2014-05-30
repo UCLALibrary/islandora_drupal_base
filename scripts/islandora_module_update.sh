b23accb211e5d0584b89d8e72f6b132c52c98fb2
Kevin S. Clarke <ksclarke@gmail.com>
2014-05-30 11:02:26 -0400

    changed module update script so it can be run from base too

--- scripts/islandora_module_update.sh
+++ scripts/islandora_module_update.sh
@@ -4,11 +4,11 @@
 #
 # Script to update modules in Islandora dir
 #
-# Description : 
+# Description :
 #	Simple helper script to pull used Islandora
 #	modules from github and update the local Islandora
 #	module Drupal dir. This is NOT required but rather
-#	should be used as a one time process when pulling 
+#	should be used as a one time process when pulling
 #	a new copy of the base Drupal repo.
 #
 #	The purpose is to restore the .git dir within each
@@ -19,12 +19,23 @@
 
 WEB_USER=apache
 WEB_GROUP=apache
+
+# Check that we're running from the project base or scripts directory
+if [[ ! -f ./islandora_modules.txt ]]; then
+    if [[ -d ./scripts ]]; then
+        cd scripts
+    else
+        echo "Needs to be run from the project or 'scripts' directory"
+        exit 1
+    fi
+fi
+
 ISLANDORA_MODULE_DIR=../drupal/sites/all/modules/islandora
 
 declare -a git_repos
 readarray git_repos < ./islandora_modules.txt
 
-for i in "${git_repos[@]}" 
+for i in "${git_repos[@]}"
 do
 	repo_basename=$(basename $i);
 	islandora_repo_dir=$ISLANDORA_MODULE_DIR/$repo_basename
