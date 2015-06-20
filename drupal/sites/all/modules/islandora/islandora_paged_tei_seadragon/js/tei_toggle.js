1ac624fb2f647b8fec7ed1f754ecc318e08f8ee1
Kevin S. Clarke <ksclarke@gmail.com>
2015-06-20 13:28:53 -0700

    Update manuscript viewer to have TEI closed by default

--- drupal/sites/all/modules/islandora/islandora_paged_tei_seadragon/js/tei_toggle.js
+++ drupal/sites/all/modules/islandora/islandora_paged_tei_seadragon/js/tei_toggle.js
@@ -4,14 +4,14 @@
             $("#paged-tei-seadragon-viewer-tei-toggle").click(function () {
                 if ($("#paged-tei-seadragon-viewer-tei").css("display") == "none") {
                     $("#paged-tei-seadragon-viewer-seadragon-pane").width("49%");
-                    $("#paged-tei-seadragon-viewer-tei-toggle").text('Hide Text');
+                    $("#paged-tei-seadragon-viewer-tei-toggle").text('Hide Transcription');
                 }
                 else {
                     // XXX: Should be something more like
                     // `width of parents-width of sibling`. But taking manual
                     // control is a can of worms with re-size.
-                    $("#paged-tei-seadragon-viewer-seadragon-pane").width("90%");
-                    $("#paged-tei-seadragon-viewer-tei-toggle").text('Show Text');
+                    $("#paged-tei-seadragon-viewer-seadragon-pane").width("98%");
+                    $("#paged-tei-seadragon-viewer-tei-toggle").text('Show Transcription');
                 }
                 $("#paged-tei-seadragon-viewer-tei").toggle();
             });
